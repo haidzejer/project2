d82f4333beee25481e227e3ab13d03c029b337eb
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-04-28 00:07:55 -0700

    made more dry

--- app/controllers/hearts_controller.rb
+++ app/controllers/hearts_controller.rb
@@ -1,16 +1,3 @@
 class HeartController < ApplicationController
-respond_to :js
 
-  def heart
-    @user = current_user
-    @post = Post.find(pararms[:id])
-    Heart.create(user_id: @user, post_id: @post)
-  end
-
-  def unheart
-    @user = current_user
-    @post = Post.find(params[:id])
-    @heart = @user.hearts.find_by_post_id(@post.id)
-    @heart.destroy
-  end
 end
